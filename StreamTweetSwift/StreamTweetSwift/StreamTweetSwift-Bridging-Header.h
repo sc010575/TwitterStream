f06eca9bef673b58b479e6f30249b7a50fb54631
Suman Chatterjee <chatterjeesuman@gmail.com>
2015-06-08 17:38:01 +0100

    Adding Coredata

---
+++ StreamTweetSwift/StreamTweetSwift/StreamTweetSwift-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  StreamTweetSwift-Bridging-Header.h
+//  StreamTweetSwift
+//
+//  Created by Suman Chatterjee on 06/06/2015.
+//  Copyright (c) 2015 Suman Chatterjee. All rights reserved.
+//
+
+#ifndef StreamTweetSwift_StreamTweetSwift_Bridging_Header_h
+#define StreamTweetSwift_StreamTweetSwift_Bridging_Header_h
+
+
+#endif
---
+++ StreamTweetSwift/TweetDetails.swift
@@ -0,0 +1,20 @@
+//
+//  TweetDetails.swift
+//  StreamTweetSwift
+//
+//  Created by Suman Chatterjee on 08/06/2015.
+//  Copyright (c) 2015 Suman Chatterjee. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class TweetDetails: NSManagedObject {
+
+    @NSManaged var imageURL: String
+    @NSManaged var createdAt: String
+    @NSManaged var tweetDescription: String
+    @NSManaged var twittURL: String
+    @NSManaged var name: String
+
+}
